48d8718c49f447d0de2a4ec12d40f828d9bb24f5
CarlosJaimeWD <carlosjaimewd@gmail.com>
2018-07-03 12:20:01 -0500

    Primera version

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+package estructuradatos;
+public class HolaMundo {
+    public static void main (String[] args) {
+        System.out.println("Hola Taller");
+        new java.util.Scanner(System.in).nextLine();
+    }
+}
